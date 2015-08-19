547858813822dfe63efbb16c36e60fe2fdebe548
brandonjw <brandonjwan@gmail.com>
2015-08-19 01:09:20 -0700

    Update mmd.py
    kalman filter update

--- mmd.py
+++ mmd.py
@@ -4,7 +4,9 @@
  python mmd.py --video videos/example_01.avi
  
  python mmd.py -v output.avi
+ 
 '''
+#from __future__ import division
 import argparse
 import datetime
 import time
@@ -15,17 +17,84 @@ ap = argparse.ArgumentParser()
 ap.add_argument("-v", "--video", help="path to the video file")
 ap.add_argument("-a", "--min-area", type=int, default=500, help="minimum area size")
 args = vars(ap.parse_args())
-f=file('mmd3.txt','w')
+f=file('mmd4.txt','w')
 img = None
-coordtext = None
 xgreater = False
 xless = False
 ygreater = False
 yless = False
+textcolor=(0,0,255)
+textcolor2=(130,70,180)
+textcolor3=(255,255,255)
+circlecolor=(0,255,0)
+circlecolor2=(255,200,0)
+circlecolor3=(255,255,255)
 #this list will contain the coords for the center of the mouse in each frame. this allows you to compare center cords, recent vs present
 #the initial values act as initializers
 centerlist=[(0,1),(2,3),(4,5),(5,6),(7,8)]
 headcoord=[(0,1), (1,2), (3,4)]
+estheadcoordlist=[(0,1),(2,3)]
+velocitylist=[(0,1),(2,3)]
+#t=.033
+still=(0,0)
+def findcenter(contours):
+    global center, radius
+    (x,y),radius = cv2.minEnclosingCircle(contours)
+    center = (int(x),int(y))
+    radius = int(radius)
+
+def highestlowestXY(list):
+    global coord12, coord12str, coord34, coord34str, coord56, coord56str, coord78, coord78str
+    sortedcordlist=sorted(list) #sort by x values
+    sortedcordlist2=sorted(list, key=lambda x: (x[0][1], x[0][0])) #sort by y values
+    
+    value1=sortedcordlist[0][0][0] #first, lowest x value
+    value2=sortedcordlist[0][0][1] ##y value to the lowest x value
+    coord12=(value1,value2)
+    coord12str=str(coord12)
+    value3=sortedcordlist[-1][0][0] #highest x value
+    value4=sortedcordlist[-1][0][1] #y value to the highest x value
+    coord34=(value3,value4)
+    coord34str=str(coord34)
+    
+    value5=sortedcordlist2[0][0][0] #x value for the lowest y value
+    value6=sortedcordlist2[0][0][1] #frst, lowest y value
+    coord56=(value5,value6)
+    coord56str=str(coord56)        
+    value7=sortedcordlist2[-1][0][0] #x value for the highest y value
+    value8=sortedcordlist2[-1][0][1] #highest y value
+    coord78=(value7,value8)
+    coord78str=str(coord78)
+
+def boundingrekt(contours):
+    (x, y, w, h) = cv2.boundingRect(c)
+    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    comma=','
+    jointhis=(str(x),str(y))
+    comma.join(jointhis)
+    cv2.putText(frame , comma.join(jointhis), (x,y),cv2.FONT_HERSHEY_SIMPLEX, .5,(0,0,255)) 
+    
+def drawcoord(frame,coord,radius,circlecolor,textcolor):
+    cv2.circle(frame,coord,radius/20,circlecolor,2)
+    cv2.putText(frame , str(coord), coord,cv2.FONT_HERSHEY_SIMPLEX, .5,textcolor)
+    
+#def standingstill(center, centerlist, coord, coordstr,textcolor, circlecolor):
+#    group1=abs(center[0] - centerlist[-4][0])
+#    group2=abs(center[1] - centerlist[-4][1])
+#    headcoord.append(coord)
+    
+def estimheadcoord(estheadcoordlist,velocitylist,headcoord,time,alpha):
+    global estheadcoordx, estheadcoordy, estheadcoord
+    estheadcoordx = (estheadcoordlist[-1][0] + velocitylist[-1][0]*time)*(1-alpha) + headcoord[-1][0]*alpha
+    estheadcoordy = (estheadcoordlist[-1][1] + velocitylist[-1][1]*time)*(1-alpha) + headcoord[-1][1]*alpha
+    estheadcoord=(int(estheadcoordx),int(estheadcoordy))
+
+def estvelocity(velocitylist,estheadcoordlist,time,alpha):
+    global estvelo
+    estvelox = (velocitylist[-1][0])*(1-alpha) + ((estheadcoordlist[-1][0] - estheadcoordlist[-2][0])/time)*alpha
+    estveloy = (velocitylist[-1][1])*(1-alpha) + ((estheadcoordlist[-1][1] - estheadcoordlist[-2][1])/time)*alpha
+    estvelo=(int(estvelox),int(estveloy))
+    
 # if the video argument is None, then we are reading from webcam
 if args.get("video", None) is None:
     camera = cv2.VideoCapture(0)
@@ -70,54 +139,24 @@ while True:
 
     # loop over the contours
     for c in cnts:
-        #        print c
         # if the contour is too small, ignore it
         if cv2.contourArea(c) < args["min_area"]:
             continue
         
         #convert c, which is an array, to a list
         clist=c.tolist()
-        sortedcordlist=sorted(clist) #sort by x values
-        sortedcordlist2=sorted(clist, key=lambda x: (x[0][1], x[0][0])) #sort by y values
+        highestlowestXY(clist)
         
-        value1=sortedcordlist[0][0][0] #first, lowest x value
-        value2=sortedcordlist[0][0][1] ##y value to the lowest x value
-        coord12=(value1,value2)
-        coord12str=str(coord12)
-        value3=sortedcordlist[-1][0][0] #highest x value
-        value4=sortedcordlist[-1][0][1] #y value to the highest x value
-        coord34=(value3,value4)
-        coord34str=str(coord34)
-        
-        value5=sortedcordlist2[0][0][0] #x value for the lowest y value
-        value6=sortedcordlist2[0][0][1] #frst, lowest y value
-        coord56=(value5,value6)
-        coord56str=str(coord56)        
-        value7=sortedcordlist2[-1][0][0] #x value for the highest y value
-        value8=sortedcordlist2[-1][0][1] #highest y value
-        coord78=(value7,value8)
-        coord78str=str(coord78)
-        
-        #finds the center of the mouse
-        (x,y),radius = cv2.minEnclosingCircle(c)
-        center = (int(x),int(y))
-        radius = int(radius)
+        findcenter(c)
         
         if center == (558,421): #won't need this if you have first frame with NO mouse
             continue
         if center[0] == 558:
             continue
-        
-        #bounding rectangle
-#        (x, y, w, h) = cv2.boundingRect(c)
-#        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-#        comma=','
-#        jointhis=(str(x),str(y))
-#        comma.join(jointhis)
-#        cv2.putText(frame , comma.join(jointhis), (x,y),cv2.FONT_HERSHEY_SIMPLEX, .5,(0,0,255))      
+            
         
         
-        #solves mouse standing still problem
+        #tries to solve mouse standing still problem
         #subtracts the present x coord from the fourth to last frame's x value
         group1=abs(center[0] - centerlist[-4][0])
         
@@ -127,31 +166,44 @@ while True:
 #        the mindset here is that if the change in both x and y values are less than 5, then the mouse is standing still
         #if standing still, then make it so that the head cords do not move (keep choosing the same direction as forward)
         if abs(group1-group2) < 5:
-            img3 = cv2.circle(frame,center,radius/20,(0,255,0),2)
-            cv2.putText(frame , str(center), center,cv2.FONT_HERSHEY_SIMPLEX, .5,(0,0,255))
+#            drawcoord(frame,center,radius,circlecolor2,textcolor2)
             centerlist.append(center)
-            
             if xgreater == True:
-                img = cv2.circle(frame,coord34,radius/20,(255,200,0),2)
-                coordtext = cv2.putText(frame , coord34str, coord34,cv2.FONT_HERSHEY_SIMPLEX, .5,(130,70,180))
+                drawcoord(frame,coord34,radius,circlecolor,textcolor)
                 headcoord.append(coord34)
+                estimheadcoord(estheadcoordlist,velocitylist,headcoord,.033,.1)
+                estheadcoordlist.append(estheadcoord)
+                estvelocity(velocitylist,estheadcoordlist,.033,.05)
+                velocitylist.append(still)
+                drawcoord(frame,estheadcoord,radius,circlecolor3,textcolor3)
                 continue
             if xless == True:
-                img = cv2.circle(frame,coord12,radius/20,(255,200,0),2)
-                coordtext = cv2.putText(frame , coord12str, coord12,cv2.FONT_HERSHEY_SIMPLEX, .5,(130,70,180))
+                drawcoord(frame,coord12,radius,circlecolor,textcolor)
                 headcoord.append(coord12)
+                estimheadcoord(estheadcoordlist,velocitylist,headcoord,.033,.1)
+                estheadcoordlist.append(estheadcoord)
+                estvelocity(velocitylist,estheadcoordlist,.033,.05)
+                velocitylist.append(still)
+                drawcoord(frame,estheadcoord,radius,circlecolor3,textcolor3)
                 continue
             if ygreater == True:
-                img = cv2.circle(frame,coord78,radius/20,(255,200,0),2)
-                coordtext = cv2.putText(frame , coord78str, coord78,cv2.FONT_HERSHEY_SIMPLEX, .5,(130,70,180))
+                drawcoord(frame,coord78,radius,circlecolor,textcolor)
                 headcoord.append(coord78)
+                estimheadcoord(estheadcoordlist,velocitylist,headcoord,.033,.1)
+                estheadcoordlist.append(estheadcoord)
+                estvelocity(velocitylist,estheadcoordlist,.033,.05)
+                velocitylist.append(still)
+                drawcoord(frame,estheadcoord,radius,circlecolor3,textcolor3)
                 continue
             if yless == True:
-                img = cv2.circle(frame,coord56,radius/20,(255,200,0),2)
-                coordtext = cv2.putText(frame , coord56str, coord56,cv2.FONT_HERSHEY_SIMPLEX, .5,(130,70,180))
+                drawcoord(frame,coord56,radius,circlecolor,textcolor)
                 headcoord.append(coord56)
+                estimheadcoord(estheadcoordlist,velocitylist,headcoord,.033,.1)
+                estheadcoordlist.append(estheadcoord)
+                estvelocity(velocitylist,estheadcoordlist,.033,.05)
+                velocitylist.append(still)
+                drawcoord(frame,estheadcoord,radius,circlecolor3,textcolor3)
                 continue
-                
             continue
         
          
@@ -182,94 +234,64 @@ while True:
                 ygreater = False
                 yless = True
                 headcoord.append(coord56)
-
+  
+                
                 
         group3=abs(headcoord[-1][0] - headcoord[-2][0])
         group4=abs(headcoord[-1][1] - headcoord[-2][1])
-        #if head coord change is greater than 60, use the opposite direction as the forward direction
-        if abs(group3 + group4) > 60:
-#            print 'jumped'
+        #if measured head coord change has a difference greater than 30, there was a large jump
+        if abs(group3 + group4) > 30:
+            estimheadcoord(estheadcoordlist,velocitylist,headcoord,.033,.1)
+            estheadcoordlist.append(estheadcoord)
+            estvelocity(velocitylist,estheadcoordlist,.033,.1)
+            velocitylist.append(estvelo)
+            drawcoord(frame,estheadcoord,radius,circlecolor3,textcolor3)
+            
             if xgreater == True:
-                img = cv2.circle(frame,coord12,radius/20,(255,200,0),2)
-                coordtext = cv2.putText(frame , coord12str, coord12,cv2.FONT_HERSHEY_SIMPLEX, .5,(130,70,180))
-                del headcoord[-1]
-                headcoord.append(coord12)
-                xgreater = False
-                xless = True
-                ygreater = False
-                yless = False
-                
-                centerlist.append(center)
-                img3 = cv2.circle(frame,center,radius/20,(0,255,0),2)
-                cv2.putText(frame , str(center), center,cv2.FONT_HERSHEY_SIMPLEX, .5,(0,0,255))
-                continue
+                drawcoord(frame,coord34,radius,circlecolor,textcolor)
             if xless == True:
-                img = cv2.circle(frame,coord34,radius/20,(255,200,0),2)
-                coordtext = cv2.putText(frame , coord34str, coord34,cv2.FONT_HERSHEY_SIMPLEX, .5,(130,70,180))
-                del headcoord[-1]
-                headcoord.append(coord34)
-                xgreater = True
-                xless = False
-                ygreater = False
-                yless = False
-                
-                centerlist.append(center)
-                img3 = cv2.circle(frame,center,radius/20,(0,255,0),2)
-                cv2.putText(frame , str(center), center,cv2.FONT_HERSHEY_SIMPLEX, .5,(0,0,255))
-                continue
+                drawcoord(frame,coord12,radius,circlecolor,textcolor)
             if ygreater == True:
-                img = cv2.circle(frame,coord56,radius/20,(255,200,0),2)
-                coordtext = cv2.putText(frame , coord56str, coord56,cv2.FONT_HERSHEY_SIMPLEX, .5,(130,70,180))
-                del headcoord[-1]
-                headcoord.append(coord56)
-                xgreater = False
-                xless = False
-                ygreater = False
-                yless = True
-                
-                centerlist.append(center)
-                img3 = cv2.circle(frame,center,radius/20,(0,255,0),2)
-                cv2.putText(frame , str(center), center,cv2.FONT_HERSHEY_SIMPLEX, .5,(0,0,255))
-                continue
+                drawcoord(frame,coord78,radius,circlecolor,textcolor)
             if yless == True:
-                img = cv2.circle(frame,coord78,radius/20,(255,200,0),2)
-                coordtext = cv2.putText(frame , coord78str, coord78,cv2.FONT_HERSHEY_SIMPLEX, .5,(130,70,180))
-                del headcoord[-1]
-                headcoord.append(coord78)
-                xgreater = False
-                xless = False
-                ygreater = True
-                yless = False
+                drawcoord(frame,coord56,radius,circlecolor,textcolor)
                 
-                centerlist.append(center)
-                img3 = cv2.circle(frame,center,radius/20,(0,255,0),2)
-                cv2.putText(frame , str(center), center,cv2.FONT_HERSHEY_SIMPLEX, .5,(0,0,255))
-                continue
-                
-                  
-        else:
-            if xgreater == True:
-                img = cv2.circle(frame,coord34,radius/20,(255,200,0),2)
-                coordtext = cv2.putText(frame , coord34str, coord34,cv2.FONT_HERSHEY_SIMPLEX, .5,(130,70,180))
+#            drawcoord(frame,center,radius,circlecolor2, textcolor2)
+            centerlist.append(center)
+            continue
+            
+            
+        if xgreater == True:
+            drawcoord(frame,coord34,radius,circlecolor,textcolor)
+        if xless == True:
+            drawcoord(frame,coord12,radius,circlecolor,textcolor)
+        if ygreater == True:
+            drawcoord(frame,coord78,radius,circlecolor,textcolor)
+        if yless == True:
+            drawcoord(frame,coord56,radius,circlecolor,textcolor)   
+        
 
-            if xless == True:
-                img = cv2.circle(frame,coord12,radius/20,(255,200,0),2)
-                coordtext = cv2.putText(frame , coord12str, coord12,cv2.FONT_HERSHEY_SIMPLEX, .5,(130,70,180))
+        
+        '''
+        higher the alpha, lower the uncertainty, more likely that the measured head coord is indeed the head coord
+        
+        estheadcoordinate = (previous estimated head coordinate + previous estimated velocity*time)*(1-alpha0) + current measured head coordinate*alpha0
+        estvelocity = (previous velocity)*(1-alpha1) + [(estimated headcoordinate from above equation - previous estimated head coordinate) / time]*alpha1
+        '''
+        #moving
+        estimheadcoord(estheadcoordlist,velocitylist,headcoord,.033,.3)
+        estheadcoordlist.append(estheadcoord)
 
-            if ygreater == True:
-                img = cv2.circle(frame,coord78,radius/20,(255,200,0),2)
-                coordtext = cv2.putText(frame , coord78str, coord78,cv2.FONT_HERSHEY_SIMPLEX, .5,(130,70,180))
+        estvelocity(velocitylist,estheadcoordlist,.033,.1)
+        velocitylist.append(estvelo)
 
-            if yless == True:
-                img = cv2.circle(frame,coord56,radius/20,(255,200,0),2)
-                coordtext = cv2.putText(frame , coord56str, coord56,cv2.FONT_HERSHEY_SIMPLEX, .5,(130,70,180))
-                
+        drawcoord(frame,estheadcoord,radius,circlecolor3,textcolor3)
 
-        centerlist.append(center)
+
 #        show center circle + coord text        
     
-        img3 = cv2.circle(frame,center,radius/20,(0,255,0),2)
-        cv2.putText(frame , str(center), center,cv2.FONT_HERSHEY_SIMPLEX, .5,(0,0,255))
+#        drawcoord(frame,center,radius,circlecolor2, textcolor2)
+        centerlist.append(center)
         
     # draw the text and timestamp on the frame
     cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
@@ -286,7 +308,7 @@ while True:
     # if the `esc` key is pressed, break from the loop
     key = cv2.waitKey(7) % 0x100
     if key == 27 or key == 10:
-        for cord in headcoord:
+        for cord in estheadcoordlist:
             f.write(str(cord))
             f.write('\n')
         f.close()
